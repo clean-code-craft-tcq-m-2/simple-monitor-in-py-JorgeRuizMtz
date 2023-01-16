9735c6079fc75d0b295227a3dc9398fe0fbdd815
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2023-01-16 13:39:08 -0600

    Add files via upload

---
+++ DL_DataStructure.py
@@ -0,0 +1,19 @@
+from Data_limitsTemp import critical_temp, warning_temp, nominal_temp, space
+from Data_limitsSOC import critical_soc, warning_soc, nominal_soc
+from Data_limitsRate import critical_chargerate, warning_chargerate, nominal_chargerate
+
+def temp_data_Structurte(temp):
+   
+   critical_temp(temp)
+   warning_temp(temp)
+   nominal_temp(temp)
+   
+def soc_data_Structurte(Datasoc):
+    critical_soc(Datasoc)
+    warning_soc(Datasoc)
+    nominal_soc(Datasoc)
+
+def ChargeRate_data_Structurte(charge_rate):
+    critical_chargerate(charge_rate)
+    warning_chargerate(charge_rate)
+    nominal_chargerate(charge_rate)
