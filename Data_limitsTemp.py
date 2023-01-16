5eff6b804725cd20e3d434fc65bd63b249996de8
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2023-01-16 13:24:40 -0600

    Add files via upload

--- Data_limitsTemp.py
+++ Data_limitsTemp.py
@@ -2,23 +2,15 @@ space = 120
 
 def critical_temp(tempdata):
     
-    if tempdata < -20:
+    if tempdata < -20 or tempdata > 50:
         print('|',''.center(space),'|',''.center(space),'|')
-        print('|','DANGER!: LOW temperature limit has been Breach!'.center(space),'|', '¡PELIGRO!: Se ha incumplido el límite de BAJA Temperatura!'.center(space),'|')
-    
-    elif tempdata > 50:
-        print('|',''.center(space),'|',''.center(space),'|')
-        print('|','DANGER!: HIGH Temperarute has been Breach!'.center(space),'|', '¡PELIGRO!: Se ha incumplido el límite de ALTA Temperatura!'.center(space),'|')
+        print('|','DANGER!: Temperature limit has been Breach!'.center(space),'|', '¡PELIGRO!: Se ha incumplido el límite de Temperatura!'.center(space),'|')
         
 def warning_temp(tempdata):
     
-    if tempdata >= -20 and tempdata <= -17.5:
+    if tempdata >= -20 and tempdata <= -17.5 or tempdata >= 47.5 and tempdata <= 50:
         print('|',''.center(space),'|',''.center(space),'|')
-        print('|','Warning!: Temperature is getting LOW'.center(space),'|', '¡ADVERTENCIA!: La temperatura está BAJANDO'.center(space),'|')
-    
-    elif tempdata >= 47.5 and tempdata <= 50:
-        print('|',''.center(space),'|',''.center(space),'|')
-        print('|','Warning!: Temperature is getting HIGH'.center(space),'|', '¡ADVERTENCIA!: La temperatura está AUMENTANDO'.center(space),'|')
+        print('|','Warning!: Temperature is changing'.center(space),'|', '¡ADVERTENCIA!: La temperatura está CAMBIANDO'.center(space),'|')
 
 def nominal_temp(tempdata):
     
