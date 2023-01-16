f00aa61924a9a5ccef561e8bc4990c0558881bfa
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2023-01-15 19:15:43 -0500

    Add files via upload

---
+++ DL_BMSchecker.py
@@ -0,0 +1,17 @@
+from DL_Battery_Tests import battery_test
+from DL_Battery_Validations import battery_validation
+        
+if __name__ == '__main__':
+    
+    battery_validation(battery_test(15, 30, 0.5))
+    battery_validation(battery_test(80, 85, 0.7))
+    battery_validation(battery_test(50, 110, 0.7))
+    battery_validation(battery_test(50, 85, 0.9))
+    battery_validation(battery_test(80, 110, 0.7))
+    battery_validation(battery_test(50, 110, 0.9))
+    battery_validation(battery_test(80, 85, 0.9))
+    battery_validation(battery_test(80, 110, 0.9))
+    
+    assert(battery_test(25, 70, 0.7)) == (True, True, True) 
+    ##assert(battery_test(50, 85, 0)) == (True, True, True)
+    
