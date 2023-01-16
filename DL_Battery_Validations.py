9dab3fdf5853fa42a88d1fe389a5ea67bae9f428
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2023-01-16 16:46:57 -0600

    Add files via upload

--- DL_Battery_Validations.py
+++ DL_Battery_Validations.py
@@ -2,14 +2,15 @@ from DL_Battery_Tests import battery_test, space
 
 test = battery_test
 
-def battery_validation(test):
-
+def AllGood_battery_validation(test):
+    
     if test== (True, True, True):
         print('|',''.center(space),'|',''.center(space),'|')
         print('|','SUMMARY:'.ljust(space),'|','RESUMEN:'.ljust(space),'|')
         print('|','Battery is working fine!: All is good!'.ljust(space), '|','La Bateria esta trabajando bien!: ¡Todo esta correcto!'.ljust(space),'|')
-    
-    elif test == (False, True, True):
+
+def Onefalse_battery_validation(test):
+    if test == (False, True, True):
         print('|',''.center(space),'|',''.center(space),'|')
         print('|','SUMMARY:'.ljust(space),'|','RESUMEN:'.ljust(space),'|')
         print('|','Check battery temperature'.ljust(space), '|','Revisar la temperatura de la Bateria'.ljust(space),'|')
@@ -24,7 +25,9 @@ def battery_validation(test):
         print('|','SUMMARY:'.ljust(space),'|','RESUMEN:'.ljust(space),'|')
         print('|','Check Charge Rate'.ljust(space), '|','Revisar Tasa de Carga'.ljust(space),'|')
         
-    elif test == (False, False, True):
+def Onetrue_battery_validation(test):
+    
+    if test == (False, False, True):
         print('|',''.center(space),'|',''.center(space),'|')
         print('|','SUMMARY:'.ljust(space),'|','RESUMEN:'.ljust(space),'|')
         print('|','Check Temperature & State of Charge. Note: Battery might be compromised'.ljust(space), '|','Revisar Temperatura & Estado de Carga. Nota: La Bateria podria estar fallando'.ljust(space),'|')
@@ -39,7 +42,8 @@ def battery_validation(test):
         print('|','SUMMARY:'.ljust(space),'|','RESUMEN:'.ljust(space),'|')
         print('|','Check Temperature & Charge Rate. Note: Battery might be compromised'.ljust(space), '|','Revisar Temperatura & Tasa de Carga. Nota: La Bateria podria estar fallando'.ljust(space),'|')
     
-    elif test == (False, False, False):
+def AllBad_battery_validation(test):
+    if test == (False, False, False):
         print('|',''.center(space),'|',''.center(space),'|')
         print('|','SUMMARY:'.ljust(space),'|','RESUMEN:'.ljust(space),'|')
         print('|','Check Temperature, State of Charge & Charge Rate. Note: Battery might be compromised and should be replaced.'.ljust(space), '|','Revisar Temperatura, Estado de Carga & Tasa de Carga. Nota: La Bateria podria estar fallando y debera ser templazada'.ljust(space),'|')
