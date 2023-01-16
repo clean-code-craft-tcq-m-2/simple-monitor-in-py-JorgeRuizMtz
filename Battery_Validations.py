dcab8505852fe462ca21a4d7b06f3b22ef2717f5
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2023-01-15 19:09:12 -0500

    Add files via upload

--- Battery_Validations.py
+++ Battery_Validations.py
@@ -7,25 +7,7 @@ def battery_validation(test):
 
     if test== (True, True, True):
         print('Battery is working fine! \nAll is good.\n')
-    
-    elif test == (False, True, True):
-        print('Check battery temperature\n')
-    
-    elif test == (True, False, True):
-        print('Check State of charge\n')
-        
-    elif test == (True, True, False):
-        print('Check Charge Rate\n')
-        
-    elif test == (False, False, True):
-        print('Check Temperature & State of Charge. Note: Battery might be compromised\n')
-    
-    elif test == (True, False, False):
-        print('Check State of Charge & Charge Rate. Note: Battery might be compromised\n')
-        
-    elif test == (False, True, False):
-        print('Check Temperature & Charge Rate. Note: Battery might be compromised\n')
-    
+
     elif test == (False, False, False):
         print('Check Temperature, State of Charge & Charge Rate. Note: Battery might be compromised and should be replaced. \n')
         
--- FunctionChecker.py
+++ FunctionChecker.py
@@ -1,6 +1,7 @@
 def temp_is_ok(temperature):
     if temperature < -20 or temperature > 50:
         print('Temperature is out of range!')
+        print('Check battery temperature\n')
         return False
     else:
         return True
@@ -8,6 +9,7 @@ def temp_is_ok(temperature):
 def soc_is_ok(soc):
     if soc < 20 or soc > 100:
         print('State of Charge is out of range!')
+        print('Check State of charge\n')
         return False
     else:
         return True
@@ -15,6 +17,7 @@ def soc_is_ok(soc):
 def change_rate_is_ok(charge_rate):
     if charge_rate < 0 or charge_rate > 0.8:
         print('Charge rate is out of range!')
+        print('Check Charge Rate\n')
         return False
     else:
         return True
