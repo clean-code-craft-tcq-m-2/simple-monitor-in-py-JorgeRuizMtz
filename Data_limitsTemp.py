d359f654da778fd60cb67a4ee0cbb0bb2eb80318
Jorge Ruiz <73081713+JorgeRuizMtz@users.noreply.github.com>
2023-01-16 13:14:10 -0600

    Add files via upload

---
+++ Data_limitsTemp.py
@@ -0,0 +1,27 @@
+space = 120
+
+def critical_temp(tempdata):
+    
+    if tempdata < -20:
+        print('|',''.center(space),'|',''.center(space),'|')
+        print('|','DANGER!: LOW temperature limit has been Breach!'.center(space),'|', '¡PELIGRO!: Se ha incumplido el límite de BAJA Temperatura!'.center(space),'|')
+    
+    elif tempdata > 50:
+        print('|',''.center(space),'|',''.center(space),'|')
+        print('|','DANGER!: HIGH Temperarute has been Breach!'.center(space),'|', '¡PELIGRO!: Se ha incumplido el límite de ALTA Temperatura!'.center(space),'|')
+        
+def warning_temp(tempdata):
+    
+    if tempdata >= -20 and tempdata <= -17.5:
+        print('|',''.center(space),'|',''.center(space),'|')
+        print('|','Warning!: Temperature is getting LOW'.center(space),'|', '¡ADVERTENCIA!: La temperatura está BAJANDO'.center(space),'|')
+    
+    elif tempdata >= 47.5 and tempdata <= 50:
+        print('|',''.center(space),'|',''.center(space),'|')
+        print('|','Warning!: Temperature is getting HIGH'.center(space),'|', '¡ADVERTENCIA!: La temperatura está AUMENTANDO'.center(space),'|')
+
+def nominal_temp(tempdata):
+    
+    if tempdata > -17.5 and tempdata < 47.5:
+        print('|',''.center(space),'|',''.center(space),'|')
+        print('|','Temperature is Nominal'.center(space),'|', 'La Temperatura es Nominal'.center(space),'|')
